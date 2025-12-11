8a0d342aa9597d45846e7d15c1fd0bd36760af09
Paxx2222 <toro2222@gmail.com>
2025-12-11 13:45:39 +0100

    Fix serial_error_handler calling non-existent method
    Changed self.serial_open() to self.connect() in serial_error_handler.
    The serial_open method does not exist in the class.

--- SampleCode/SOLOMotorControllersUart.py
+++ SampleCode/SOLOMotorControllersUart.py
@@ -206,7 +206,7 @@ class SoloMotorControllerUart:
                 self._logger.error( e, exc_info=True)
 
             self._logger.debug('SEH: serial init')
-            res = self.serial_open()
+            res = self.connect()
             self._logger.debug('SEH end')
             return res
 
